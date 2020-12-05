89209e08bda396816f1a1c13353f02725326c554
Emil <emilkhazioff@yandex.com>
2020-12-05 20:44:49 +0500

    realese v1

---
+++ weather.py
@@ -0,0 +1,31 @@
+import eel
+import requests
+
+'''import os
+import sys
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    if getattr(sys, 'frozen', False):
+        base_path = sys._MEIPASS
+    else:
+        base_path = os.getcwd()
+    return os.path.join(base_path, relative_path)'''
+
+@eel.expose
+def what_weather(city):
+    url = f'http://wttr.in/{city}'
+    weather_parameters = {
+        'format': 2,
+        'M': ''
+    }
+    try:
+        response = requests.get(url, params=weather_parameters)
+    except requests.ConnectionError:
+        return '<сетевая ошибка>'
+    if response.status_code == 200:
+        return response.text.strip()
+    else:
+        return 'Ошибка на сервере погоды:('
+
+eel.init("D:\grid\weather\web")
+eel.start("weather.html", size=(700,700))
